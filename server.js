46b5e66e13e7c05b5276142053988387a98c5cd4
Your GitHub Username <Mankalivijayababu>
2026-02-27 19:08:03 +0530

    Fix production API and socket1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+
 const http = require("http");
 const { Server } = require("socket.io");
 const userRoutes = require("./routes/userRoutes");
